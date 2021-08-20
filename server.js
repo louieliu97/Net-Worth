22b1f79e2a37320d54e3fb390be474d2c904214f
Louie <liu.lo@husky.neu.edu>
2021-08-19 21:29:21 -0400

    Working example

---
+++ server.js
@@ -0,0 +1,52 @@
+console.log('Server-side code running');
+
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const app = express();
+
+// serve files from the public directory
+app.use(express.static('public'));
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'node-express-test';
+
+let db;
+
+MongoClient.connect(url, function (err, client) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
+    db = client.db(dbName);
+
+    // start the express web server listening on 8080
+    app.listen(8080, () => {
+        console.log('listening on 8080');
+    });
+});
+
+// serve the homepage
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/clicked', (req, res) => {
+    const click = { clickTime: new Date() };
+    console.log(click);
+    console.log(db);
+
+    const collection = db.collection('clicks');
+    collection.insertOne(click, function (err, result) {
+        assert.equal(err, null)
+        console.log("inserted one click")
+        res.sendStatus(201);
+    })
+})
+
+// get the click data from the database
+app.get('/clicks', (req, res) => {
+    db.collection('clicks').find().toArray((err, result) => {
+        if (err) return console.log(err);
+        res.send(result);
+    });
+});
