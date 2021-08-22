55dfb3f88075b57b05d457a0bbd64fa6fe7939b2
Louie <liu.lo@husky.neu.edu>
2021-08-22 18:15:33 -0400

    2 separate js for bar + pie. Working examples of both

--- public/app.js
+++ public/app.js
@@ -1,9 +1,104 @@
 ﻿// app.js
 console.log('Client-side code running');
 
+// set the dimensions and margins of the graph
+const pie_margin = 100;
+const pie_width = 600 - pie_margin;
+const pie_height = 600 - pie_margin;
+
+const radius = Math.min(pie_width, pie_height) / 2 - pie_margin;
+
+// append the container for the graph to the page
+
+// append the svg object to the body of the page
+// append a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+var asset_pie = d3.select("#asset-pie")
+    .append('svg')
+    .attr("width", pie_width)
+    .attr("height", pie_height)
+    .append('g')
+    .attr("transform", `translate(${pie_width / 2}, ${pie_height / 2})`);
+
+var pie = d3.pie()
+    .sort(null)
+    .value(function (d) { return d[1]; })
+
+var pie_arc = d3.arc()
+    .innerRadius(radius * 0.5)
+    .outerRadius(radius * 0.8)
+
+var pie_outerArc = d3.arc()
+    .innerRadius(radius * 0.9)
+    .outerRadius(radius * 0.9)
+
+const color = d3.scaleOrdinal()
+    .domain(["cash", "crypto", "investments"])
+    .range(d3.schemeDark2);
+
+// Get the poll data from the `/poll` endpoint
+async function fetchAssets() {
+    fetch('http://localhost:8080/assets')
+        .then(response => response.json())
+        .then(assets => {
+            console.log(JSON.stringify(assets));
+            const data_ready = pie(Object.entries(assets));
+            updatePie(data_ready, pie_arc, pie_outerArc);
+            return data_ready;
+        });
+}
+
+function updatePie(data_ready, pie_arc, pie_outerArc) {
+    // remove all old properties
+    asset_pie.selectAll("g > *").remove();
+    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+    asset_pie
+        .selectAll('allSlices')
+        .data(data_ready)
+        .join('path')
+        .attr('d', pie_arc)
+        .attr('fill', d => color(d.data[0]))
+        .attr("stroke", "white")
+        .style("stroke-width", "2px")
+        .style("opacity", 0.7)
+
+    // Add the polylines between chart and labels:
+    asset_pie
+        .selectAll('allPolylines')
+        .data(data_ready)
+        .join('polyline')
+        .attr("stroke", "black")
+        .style("fill", "none")
+        .attr("stroke-width", 1)
+        .attr('points', function (d) {
+            const posA = pie_arc.centroid(d) // line insertion in the slice
+            const posB = pie_outerArc.centroid(d) // line break: we use the other arc generator that has been built only for that
+            const posC = pie_outerArc.centroid(d); // Label position = almost the same as posB
+            const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 // we need the angle to see if the X position will be at the extreme right or extreme left
+            posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
+            return [posA, posB, posC]
+        })
+
+    // Add the polylines between chart and labels:
+    asset_pie
+        .selectAll('allLabels')
+        .data(data_ready)
+        .join('text')
+        .text(d => d.data[0] + " $" + d.data[1])
+        .attr('transform', function (d) {
+            const pos = pie_outerArc.centroid(d);
+            const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+            pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
+            return `translate(${pos})`;
+        })
+        .style('text-anchor', function (d) {
+            const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+            return (midangle < Math.PI ? 'start' : 'end')
+        })
+}
+
 const insertButton = document.getElementById('insertButton');
 insertButton.addEventListener('click', function (e) {
-    console.log('insertButton was clicked');
     var assetSelect = document.getElementById('assetSelect');
     var assetName = document.getElementById('assetName');
     var accountName = document.getElementById('accountName');
@@ -14,8 +109,10 @@ insertButton.addEventListener('click', function (e) {
     var accountNameVal = String(accountName.value);
     var accountValueVal = parseFloat(accountValue.value);
 
+    // resets values to empty
     accountName.value = "";
     accountValue.value = "";
+    assetName.value = "";
 
     const response = {
         assetType: assetTypeVal,
@@ -29,164 +126,10 @@ insertButton.addEventListener('click', function (e) {
         method: 'POST',
         body: JSON.stringify(response),
         headers: { 'Content-Type': 'application/json' }
-    }).then(res => res.json())
-        .then(json => console.log("response: " + json))
+    }).then(res => fetchAssets())
         .catch(function (error) {
             console.log(error);
         });
 });
 
-const queryButton = document.getElementById('queryButton');
-queryButton.addEventListener('click', function (e) {
-    console.log('queryButton was clicked');
-
-    var tName = String(document.getElementById('tableSelect').value);
-    const response = {
-        tableName: tName,
-    };
-
-    const queryTextArea = document.getElementById('queryTextArea');
-    fetch('/query', {
-        method: 'POST',
-        body: JSON.stringify(response),
-        headers: { 'Content-Type': 'application/json' }
-    }).then(res => res.json())
-        .then(function (json) {
-            var queryTextAreaStr = "";
-            for (let i = 0; i < json.length; i++) {
-                const j = JSON.parse(json[i]);
-                queryTextAreaStr += "-------------------------------\r\n";
-                queryTextAreaStr += "insert Time: " + j["insertTime"] + "\r\n";
-                queryTextAreaStr += "account Value: " + j["accountvalue"] + "\r\n";
-                queryTextAreaStr += "account Name: " + j["accountName"] + "\r\n";
-                queryTextAreaStr += "\r\n-------------------------------\r\n";
-            }
-            queryTextArea.value = queryTextAreaStr;
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-});
-
-const addVoteButton = document.getElementById('addVoteButton');
-addVoteButton.addEventListener('click', function (e) {
-    var teamName = String(document.getElementById('pollSelect').value);
-    const response = {
-        name: teamName
-    };
-
-    console.log("Sending team name: " + teamName);
-    fetch('/addPoll', {
-        method: 'POST',
-        body: JSON.stringify(response),
-        headers: { 'Content-Type': 'application/json' }
-    }).then(res => fetchPollText())
-        .catch(function (err) {
-            console.error(err);
-        })
-});
-
-// set the dimensions and margins of the graph
-const margin = 200;
-const width = 1000 - margin;
-const height = 1000 - margin;
-
-const radius = Math.min(width, height) / 2 - margin;
-
-// append the container for the graph to the page
-const container = d3
-    .select('body')
-    .append('div')
-    .attr('class', 'container');
-
-container.append('h1').text('Who will win the 2018/19 Premier League Season?');
-
-// append the svg object to the body of the page
-// append a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
-var svg = container
-    .append('svg')
-    .attr("width", width)
-    .attr("height", height)
-    .append('g')
-    .attr("transform", `translate(${width / 2}, ${height / 2})`);
-
-var pie = d3.pie()
-    .sort(null)
-    .value(function (d) { return d[1]; })
-
-var arc = d3.arc()
-    .innerRadius(radius * 0.5)
-    .outerRadius(radius * 0.8)
-
-var outerArc = d3.arc()
-    .innerRadius(radius * 0.9)
-    .outerRadius(radius * 0.9)
-
-const color = d3.scaleOrdinal()
-    .domain(["Arsenal", "Chelsea", "Liverpool", "Manchester City", "Manchester United"])
-    .range(d3.schemeDark2);
-
-// Get the poll data from the `/poll` endpoint
-async function fetchPollText() {
-    fetch('http://localhost:8080/poll')
-        .then(response => response.json())
-        .then(poll => {
-            console.log("Poll: " + JSON.stringify(poll));
-            const data_ready = pie(Object.entries(poll));
-            console.log(data_ready);
-            update(data_ready, arc, outerArc);
-            return data_ready;
-        });
-}
-
-function update(data_ready, arc, outerArc) {
-    d3.selectAll("g > *").remove();
-    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-    svg
-        .selectAll('allSlices')
-        .data(data_ready)
-        .join('path')
-        .attr('d', arc)
-        .attr('fill', d => color(d.data[0]))
-        .attr("stroke", "white")
-        .style("stroke-width", "2px")
-        .style("opacity", 0.7)
-
-    // Add the polylines between chart and labels:
-    svg
-        .selectAll('allPolylines')
-        .data(data_ready)
-        .join('polyline')
-        .attr("stroke", "black")
-        .style("fill", "none")
-        .attr("stroke-width", 1)
-        .attr('points', function (d) {
-            const posA = arc.centroid(d) // line insertion in the slice
-            const posB = outerArc.centroid(d) // line break: we use the other arc generator that has been built only for that
-            const posC = outerArc.centroid(d); // Label position = almost the same as posB
-            const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 // we need the angle to see if the X position will be at the extreme right or extreme left
-            posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
-            return [posA, posB, posC]
-        })
-
-    // Add the polylines between chart and labels:
-    svg
-        .selectAll('allLabels')
-        .data(data_ready)
-        .join('text')
-        .text(d => d.data[0] + " " + d.data[1] + " votes")
-        .attr('transform', function (d) {
-            const pos = outerArc.centroid(d);
-            const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
-            pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
-            return `translate(${pos})`;
-        })
-        .style('text-anchor', function (d) {
-            const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
-            return (midangle < Math.PI ? 'start' : 'end')
-        })
-    console.log("Done");
-}
-
-fetchPollText();
+fetchAssets();
--- server.js
+++ server.js
@@ -34,63 +34,208 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-const constructPoll = async (db) => {
+const constructAssets = async (db) => {
     try {
-        const poll_test = db.collection("poll_test");
-        var arr = {};
-        const items = await poll_test.find().toArray();
+        const assets_db = db.collection("assets");
+        var json = {};
+        const items = await assets_db.find().toArray();
+
         for (let i = 0; i < items.length; i++) {
-            arr[items[i].name] = items[i].votes;
+            json[items[i].asset_type] = items[i].value;
         }
-        return arr;
+        return json;
     }
     catch (err) { console.error(err); }
 }
 
-app.get('/poll', async function (req, res) {
-    var poll = await constructPoll(db);
+app.get('/assets', async function (req, res) {
+    var poll = await constructAssets(db);
     res.json(poll);
 });
 
-app.post('/addPoll', async function (req, res) {
-    try {
-        var teamName = String(req.body.name);
-        const poll_test = db.collection("poll_test");
-        var update = {
-            $inc: {
-                votes: 1
-            }
-        }
-        const items = await poll_test.updateOne(
-            { name: teamName }, update);
-        res.sendStatus(201);
-    }
-    catch (err) { console.error(err); } // catch any mongo error here
-});
-
-const getAssetAccount = async (db, assetTypeVal, accountNameVal) => {
+const getAssetAccount = async (db, assetType, accountName) => {
     try {
         const asset_accounts = db.collection("asset_accounts");
 
         const items = await asset_accounts.find({
-            asset_type: assetTypeVal,
-            account_name: accountNameVal,
+            asset_type: assetType,
+            account_name: accountName,
         });
         return items.toArray();
     }
     catch (err) { console.error(err); } // catch any mongo error here
 }
 
-const getAssetAccountItem = async (db, assetTypeVal, accountNameVal, assetNameVal) => {
+const getAssetAccountItem = async (db, assetType, accountName, assetName) => {
     try {
         const asset_accounts_items = await db.collection("asset_accounts_items");
 
         const items = await asset_accounts_items.find({
-            asset_type: assetTypeVal,
-            account_name: accountNameVal,
-            asset_name: assetNameVal
+            asset_type: assetType,
+            account_name: accountName,
+            asset_name: assetName
         });
         return items.toArray();
+    }
+    catch (err) { console.error(err); } // catch any mongo error here
+}
+
+const incAsset = async (db, assetType, accountValue) => {
+    try {
+        const asset_db = db.collection("assets");
+        const query = { asset_type: assetType };
+        var item = await asset_db.findOne(query);
+
+        if (item == null) {
+            const asset_obj = {
+                asset_type: assetType,
+                value: accountValue
+            }
+            asset_db
+                .insertOne(asset_obj)
+                .then(
+                    res => console.log("Added to assets"),
+                    err => console.error(`Something went wrong: ${err}`)
+                );
+        } else {
+            var replace = {
+                $inc: {
+                    value: accountValue
+                }
+            }
+            asset_db
+                .updateOne(query, replace)
+                .then(
+                    res => console.log("Updated assets"),
+                    err => console.error(`Something went wrong: ${err}`)
+                );
+        }
+    }
+    catch (err) { console.error(err); } // catch any mongo error here
+}
+
+const incAssetAccount = async (db, assetType, accountName, accountValue) => {
+
+    try {
+        const asset_account_db = db.collection("asset_accounts");
+        let curr_account_total = 0;
+        var account_assets = await getAssetAccount(db, assetType, accountName);
+        for (let i = 0; i < account_assets.length; i++)
+            curr_account_total += parseFloat(account_assets[i].value);
+
+        if (curr_account_total == 0) {
+            const asset_account = {
+                asset_type: assetType,
+                account_name: accountName,
+                last_transaction_date: new Date(),
+                value: accountValue,
+                dollar_value: accountValue,
+            }
+
+            asset_account_db
+                .insertOne(asset_account)
+                .then(
+                    res => console.log("Added to asset_account"),
+                    err => console.error(`Something went wrong: ${err}`)
+                );
+            // modify the total value to add value
+        } else {
+            curr_account_total += accountValue;
+            var query = {
+                asset_type: assetType,
+                account_name: accountName
+            }
+            var replace = {
+                $set: {
+                    asset_type: assetType,
+                    account_name: accountName,
+                    last_transaction_date: new Date(),
+                    value: mongodb.Double(curr_account_total),
+                    dollar_value: mongodb.Double(curr_account_total)
+                }
+            }
+            asset_account_db
+                .updateOne(query, replace)
+                .then(
+                    res => console.log("Updated asset_account"),
+                    err => console.error(`Something went wrong: ${err}`)
+                );
+        }
+    }
+    catch (err) { console.error(err); } // catch any mongo error here
+}
+
+const incAssetTransactions = async (db, assetType, assetName, accountName, accountValue) => {
+    try {
+        const asset_transactions_db = db.collection("asset_transactions");
+        var asset_transaction = {
+            asset_type: assetType,
+            asset_name: assetName,
+            account_name: accountName,
+            date: new Date(),
+            value: accountValue,
+            dollar_value: accountValue,
+        }
+        asset_transactions_db
+            .insertOne(asset_transaction)
+            .then(
+                res => console.log("Updated asset_account_transaction"),
+                err => console.error(`Something went wrong: ${err}`)
+            );
+    }
+    catch (err) { console.error(err); } // catch any mongo error here
+}
+
+const incAssetAccountItems = async (db, assetType, assetName, accountName, accountValue) => {
+    try {
+        const asset_account_item_db = db.collection("asset_accounts_items");
+
+        let asset_account_total = 0;
+        var asset_account_item = await getAssetAccountItem(db, assetType, accountName, assetName);
+        for (let i = 0; i < asset_account_item.length; i++) {
+            asset_account_total += parseFloat(asset_account_item[i].value);
+        }
+        // If first time inserting this asset for this account, insert for first time
+        if (asset_account_total == 0) {
+            const asset_account_item = {
+                asset_type: assetType,
+                account_name: accountName,
+                asset_name: assetName,
+                last_transaction_date: new Date(),
+                value: accountValue,
+                dollar_value: accountValue
+            }
+            asset_account_item_db
+                .insertOne(asset_account_item)
+                .then(
+                    res => console.log("Added to asset_account_item"),
+                    err => console.error(`Something went wrong: ${err}`)
+                );
+            // otherwise modify value
+        } else {
+            asset_account_total += accountValue;
+            var query = {
+                asset_type: assetType,
+                account_name: accountName,
+                asset_name: assetName
+            }
+            var replace = {
+                $set: {
+                    asset_type: assetType,
+                    account_name: accountName,
+                    asset_name: assetName,
+                    last_transaction_date: new Date(),
+                    value: mongodb.Double(asset_account_total),
+                    dollar_value: mongodb.Double(asset_account_total)
+                }
+            }
+            asset_account_item_db
+                .updateOne(query, replace)
+                .then(
+                    res => console.log("Updated asset_account_item"),
+                    err => console.error(`Something went wrong: ${err}`)
+                );
+        }
     }
     catch (err) { console.error(err); } // catch any mongo error here
 }
@@ -101,124 +246,15 @@ app.post('/insert', async function (req, res) {
     var accountName = req.body.accountName;
     var accountValue = mongodb.Double(req.body.accountValue);
 
-    const asset_account_db = db.collection("asset_accounts");
-
-    let curr_account_total = 0;
-    var account_assets = await getAssetAccount(db, assetType, accountName);
-    for (let i = 0; i < account_assets.length; i++)
-        curr_account_total += parseFloat(account_assets[i].value);
-
-    if (curr_account_total == 0) {
-        const asset_account = {
-            asset_type: assetType,
-            account_name: accountName,
-            last_transaction_date: new Date(),
-            value: accountValue,
-            dollar_value: accountValue,
-        }
-
-        console.log("asset_acount new: " + JSON.stringify(asset_account));
-        asset_account_db
-            .insertOne(asset_account)
-            .then(
-                res => console.log("Added to asset_account"),
-                err => console.error(`Something went wrong: ${err}`)
-            );
-        // modify the total value to add value
-    } else {
-        curr_account_total += accountValue;
-        var query = {
-            asset_type: assetType,
-            account_name: accountName
-        }
-        var replace = {
-            $set: {
-                asset_type: assetType,
-                account_name: accountName,
-                last_transaction_date: new Date(),
-                value: mongodb.Double(curr_account_total),
-                dollar_value: mongodb.Double(curr_account_total)
-            }
-        }
-        asset_account_db
-            .updateOne(query, replace)
-            .then(
-                res => console.log("Updated asset_account"),
-                err => console.error(`Something went wrong: ${err}`)
-            );
-    }
-            
-    const asset_transactions_db = db.collection("asset_transactions");
-    var asset_transaction = {
-        asset_type: assetType,
-        asset_name: assetName,
-        account_name: accountName,
-        date: new Date(),
-        value: accountValue,
-        dollar_value: accountValue,
-    }
-    asset_transactions_db
-        .insertOne(asset_transaction)
-        .then(
-            res => console.log("Updated asset_account_transaction"),
-            err => console.error(`Something went wrong: ${err}`)
-        );
-        // modify the total value to add value
-
-
-    const asset_account_item_db = db.collection("asset_accounts_items");
-
-    let asset_account_total = 0;
-    var asset_account_item = await getAssetAccountItem(db, assetType, accountName, assetName);
-    for (let i = 0; i < asset_account_item.length; i++) {
-        asset_account_total += parseFloat(asset_account_item[i].value);
-    }
-    // If first time inserting this asset for this account, insert for first time
-    if (asset_account_total == 0) {
-        const asset_account_item = {
-            asset_type: assetType,
-            account_name: accountName,
-            asset_name: assetName,
-            last_transaction_date: new Date(),
-            value: accountValue,
-            dollar_value: accountValue
-        }
-        asset_account_item_db
-            .insertOne(asset_account_item)
-            .then(
-                res => console.log("Added to asset_account_item"),
-                err => console.error(`Something went wrong: ${err}`)
-            );
-    // otherwise modify value
-    } else {
-        asset_account_total += accountValue;
-        var query = {
-            asset_type: assetType,
-            account_name: accountName,
-            asset_name: assetName
-        }
-        var replace = {
-            $set: {
-                asset_type: assetType,
-                account_name: accountName,
-                asset_name: assetName,
-                last_transaction_date: new Date(),
-                value: mongodb.Double(asset_account_total),
-                dollar_value: mongodb.Double(asset_account_total)
-            }
-        }
-        asset_account_item_db
-            .updateOne(query, replace)
-            .then(
-                res => console.log("Updated asset_account_item"),
-                err => console.error(`Something went wrong: ${err}`)
-            );
-    }
+    await Promise.allSettled([incAsset(db, assetType, accountValue),
+    incAssetAccount(db, assetType, accountName, accountValue),
+    incAssetTransactions(db, assetType, assetName, accountName, accountValue),
+    incAssetAccountItems(db, assetType, assetName, accountName, accountValue)]);
+   
     res.sendStatus(201);
 })
 
 app.post('/query', (req, res) => {
-    console.log("Querying from database");
     var tableName = String(req.body.tableName);
 
     const collection = db.collection(tableName);
@@ -229,7 +265,6 @@ app.post('/query', (req, res) => {
         array.push(str);
     });
     stream.on("end", function () {
-        console.log(JSON.stringify(array));
         res.send(JSON.stringify(array));
     });
 
